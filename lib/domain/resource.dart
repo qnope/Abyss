2c6c98f06b7fba320e335bc7c6537dfb0ffcd532
Antoine Morrier <antoine.morrier@gmail.com>
2026-03-29 19:18:46 +0200

    game-screen-layout#02 Create Resource domain model
    Add Resource HiveObject with type, amount, productionPerTurn, and
    maxStorage fields (typeId: 3).

---
+++ lib/domain/resource.dart
@@ -0,0 +1,26 @@
+import 'package:hive/hive.dart';
+import 'resource_type.dart';
+
+part 'resource.g.dart';
+
+@HiveType(typeId: 3)
+class Resource extends HiveObject {
+  @HiveField(0)
+  final ResourceType type;
+
+  @HiveField(1)
+  int amount;
+
+  @HiveField(2)
+  final int productionPerTurn;
+
+  @HiveField(3)
+  final int maxStorage;
+
+  Resource({
+    required this.type,
+    required this.amount,
+    this.productionPerTurn = 0,
+    this.maxStorage = 500,
+  });
+}
---
+++ lib/domain/resource.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'resource.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ResourceAdapter extends TypeAdapter<Resource> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Resource read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Resource(
+      type: fields[0] as ResourceType,
+      amount: fields[1] as int,
+      productionPerTurn: fields[2] as int,
+      maxStorage: fields[3] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Resource obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.type)
+      ..writeByte(1)
+      ..write(obj.amount)
+      ..writeByte(2)
+      ..write(obj.productionPerTurn)
+      ..writeByte(3)
+      ..write(obj.maxStorage);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ResourceAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}

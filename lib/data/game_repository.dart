0c450d018a7398ff5fb286152c942f20c24841ac
Antoine Morrier <antoine.morrier@gmail.com>
2026-03-29 19:18:54 +0200

    game-screen-layout#04 Register new Hive adapters in GameRepository
    Register ResourceTypeAdapter and ResourceAdapter before Player/Game
    adapters for proper dependency ordering.

--- lib/data/game_repository.dart
+++ lib/data/game_repository.dart
@@ -1,12 +1,16 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import '../domain/game.dart';
 import '../domain/player.dart';
+import '../domain/resource.dart';
+import '../domain/resource_type.dart';
 
 class GameRepository {
   static const _boxName = 'games';
 
   static Future<void> initialize() async {
     await Hive.initFlutter();
+    Hive.registerAdapter(ResourceTypeAdapter());
+    Hive.registerAdapter(ResourceAdapter());
     Hive.registerAdapter(PlayerAdapter());
     Hive.registerAdapter(GameAdapter());
     await Hive.openBox<Game>(_boxName);

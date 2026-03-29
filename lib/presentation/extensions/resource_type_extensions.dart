3c8f9229b5b8f6a0492fe92055e4f7188040f944
Antoine Morrier <antoine.morrier@gmail.com>
2026-03-29 19:18:59 +0200

    game-screen-layout#05 Create ResourceBarItem widget and type extensions
    Add ResourceTypeColor and ResourceTypeInfo extensions for color mapping
    and display names. Create ResourceBarItem showing icon, amount, and
    production rate per turn.

---
+++ lib/presentation/extensions/resource_type_extensions.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../domain/resource_type.dart';
+import '../theme/abyss_colors.dart';
+
+extension ResourceTypeColor on ResourceType {
+  Color get color => switch (this) {
+    ResourceType.algae => AbyssColors.algaeGreen,
+    ResourceType.coral => AbyssColors.coralPink,
+    ResourceType.ore => AbyssColors.oreBlue,
+    ResourceType.energy => AbyssColors.energyYellow,
+    ResourceType.pearl => AbyssColors.pearlWhite,
+  };
+}
+
+extension ResourceTypeInfo on ResourceType {
+  String get displayName => switch (this) {
+    ResourceType.algae => 'Algues',
+    ResourceType.coral => 'Corail',
+    ResourceType.ore => 'Minerai',
+    ResourceType.energy => 'Énergie',
+    ResourceType.pearl => 'Perles',
+  };
+
+  String get flavorText => switch (this) {
+    ResourceType.algae => 'Nourriture cultivée dans les fermes sous-marines pour nourrir vos unités.',
+    ResourceType.coral => 'Matériau de construction récolté sur les récifs pour bâtir votre base.',
+    ResourceType.ore => 'Métal extrait des profondeurs pour forger des équipements avancés.',
+    ResourceType.energy => 'Énergie captée pour alimenter vos bâtiments et machines.',
+    ResourceType.pearl => 'Gemmes rares découvertes lors d\'explorations, essentielles aux technologies avancées.',
+  };
+}
---
+++ lib/presentation/widgets/resource_bar_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../../domain/resource.dart';
+import '../extensions/resource_type_extensions.dart';
+import 'animated_resource_amount.dart';
+import 'resource_icon.dart';
+
+class ResourceBarItem extends StatelessWidget {
+  final Resource resource;
+  final VoidCallback? onTap;
+
+  const ResourceBarItem({
+    super.key,
+    required this.resource,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color = resource.type.color;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ResourceIcon(type: resource.type, size: 20),
+          const SizedBox(width: 4),
+          AnimatedResourceAmount(
+            amount: resource.amount,
+            baseColor: color,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            ),
+          ),
+          if (resource.productionPerTurn > 0) ...[
+            const SizedBox(width: 2),
+            Text(
+              '+${resource.productionPerTurn}/t',
+              style: TextStyle(
+                color: color.withValues(alpha: 0.7),
+                fontSize: 11,
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}

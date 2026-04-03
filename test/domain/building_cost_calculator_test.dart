24549e673fbc73e74e61ed140495ffc6d13674d0
Antoine Morrier <antoine.morrier@gmail.com>
2026-04-03 23:35:41 +0200

    production-buildings#06 Add unit tests for production buildings in BuildingCostCalculator

--- test/domain/building_cost_calculator_test.dart
+++ test/domain/building_cost_calculator_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:abyss/domain/building.dart';
 import 'package:abyss/domain/building_cost_calculator.dart';
 import 'package:abyss/domain/building_type.dart';
-import 'package:abyss/domain/resource.dart';
 import 'package:abyss/domain/resource_type.dart';
 
 void main() {
@@ -14,47 +12,81 @@ void main() {
 
   group('upgradeCost', () {
     test('HQ level 0->1: coral=30, ore=20', () {
-      final cost = calculator.upgradeCost(
-        BuildingType.headquarters,
-        0,
-      );
+      final cost = calculator.upgradeCost(BuildingType.headquarters, 0);
       expect(cost[ResourceType.coral], 30);
       expect(cost[ResourceType.ore], 20);
     });
 
     test('HQ level 1->2: coral=60, ore=40', () {
-      final cost = calculator.upgradeCost(
-        BuildingType.headquarters,
-        1,
-      );
+      final cost = calculator.upgradeCost(BuildingType.headquarters, 1);
       expect(cost[ResourceType.coral], 60);
       expect(cost[ResourceType.ore], 40);
     });
 
     test('HQ level 5->6: coral=780, ore=520', () {
-      final cost = calculator.upgradeCost(
-        BuildingType.headquarters,
-        5,
-      );
+      final cost = calculator.upgradeCost(BuildingType.headquarters, 5);
       expect(cost[ResourceType.coral], 780);
       expect(cost[ResourceType.ore], 520);
     });
 
     test('HQ level 9->10: coral=2460, ore=1640', () {
-      final cost = calculator.upgradeCost(
-        BuildingType.headquarters,
-        9,
-      );
+      final cost = calculator.upgradeCost(BuildingType.headquarters, 9);
       expect(cost[ResourceType.coral], 2460);
       expect(cost[ResourceType.ore], 1640);
     });
 
     test('HQ at max level (10): returns empty map', () {
-      final cost = calculator.upgradeCost(
-        BuildingType.headquarters,
-        10,
-      );
+      final cost = calculator.upgradeCost(BuildingType.headquarters, 10);
       expect(cost, isEmpty);
+    });
+
+    test('algaeFarm level 0->1: coral=20', () {
+      final cost = calculator.upgradeCost(BuildingType.algaeFarm, 0);
+      expect(cost[ResourceType.coral], 20);
+    });
+
+    test('algaeFarm level 2->3: coral=100', () {
+      final cost = calculator.upgradeCost(BuildingType.algaeFarm, 2);
+      expect(cost[ResourceType.coral], 100);
+    });
+
+    test('algaeFarm at max level 5: empty', () {
+      final cost = calculator.upgradeCost(BuildingType.algaeFarm, 5);
+      expect(cost, isEmpty);
+    });
+
+    test('coralMine level 0->1: ore=15', () {
+      final cost = calculator.upgradeCost(BuildingType.coralMine, 0);
+      expect(cost[ResourceType.ore], 15);
+    });
+
+    test('coralMine level 3->4: ore=150', () {
+      final cost = calculator.upgradeCost(BuildingType.coralMine, 3);
+      expect(cost[ResourceType.ore], 150);
+    });
+
+    test('oreExtractor level 0->1: coral=25, energy=15', () {
+      final cost = calculator.upgradeCost(BuildingType.oreExtractor, 0);
+      expect(cost[ResourceType.coral], 25);
+      expect(cost[ResourceType.energy], 15);
+    });
+
+    test('oreExtractor level 1->2: coral=50, energy=30', () {
+      final cost = calculator.upgradeCost(BuildingType.oreExtractor, 1);
+      expect(cost[ResourceType.coral], 50);
+      expect(cost[ResourceType.energy], 30);
+    });
+
+    test('solarPanel level 0->1: coral=20, ore=15', () {
+      final cost = calculator.upgradeCost(BuildingType.solarPanel, 0);
+      expect(cost[ResourceType.coral], 20);
+      expect(cost[ResourceType.ore], 15);
+    });
+
+    test('solarPanel level 4->5: coral=340, ore=255', () {
+      final cost = calculator.upgradeCost(BuildingType.solarPanel, 4);
+      expect(cost[ResourceType.coral], 340);
+      expect(cost[ResourceType.ore], 255);
     });
   });
 
@@ -62,153 +94,21 @@ void main() {
     test('HQ is 10', () {
       expect(calculator.maxLevel(BuildingType.headquarters), 10);
     });
-  });
 
-  group('prerequisites', () {
-    test('HQ has no prerequisites', () {
-      final prereqs = calculator.prerequisites(
-        BuildingType.headquarters,
-        1,
-      );
-      expect(prereqs, isEmpty);
-    });
-  });
-
-  group('checkUpgrade', () {
-    test('can upgrade when resources sufficient', () {
-      final resources = {
-        ResourceType.coral: Resource(
-          type: ResourceType.coral,
-          amount: 100,
-        ),
-        ResourceType.ore: Resource(
-          type: ResourceType.ore,
-          amount: 100,
-        ),
-      };
-      final buildings = {
-        BuildingType.headquarters:
-            Building(type: BuildingType.headquarters, level: 0),
-      };
-
-      final result = calculator.checkUpgrade(
-        type: BuildingType.headquarters,
-        currentLevel: 0,
-        resources: resources,
-        allBuildings: buildings,
-      );
-
-      expect(result.canUpgrade, isTrue);
-      expect(result.isMaxLevel, isFalse);
-      expect(result.missingResources, isEmpty);
-      expect(result.missingPrerequisites, isEmpty);
+    test('algaeFarm is 5', () {
+      expect(calculator.maxLevel(BuildingType.algaeFarm), 5);
     });
 
-    test('cannot upgrade when coral insufficient', () {
-      final resources = {
-        ResourceType.coral: Resource(
-          type: ResourceType.coral,
-          amount: 10,
-        ),
-        ResourceType.ore: Resource(
-          type: ResourceType.ore,
-          amount: 100,
-        ),
-      };
-      final buildings = {
-        BuildingType.headquarters:
-            Building(type: BuildingType.headquarters, level: 0),
-      };
-
-      final result = calculator.checkUpgrade(
-        type: BuildingType.headquarters,
-        currentLevel: 0,
-        resources: resources,
-        allBuildings: buildings,
-      );
-
-      expect(result.canUpgrade, isFalse);
-      expect(result.missingResources[ResourceType.coral], 20);
+    test('coralMine is 5', () {
+      expect(calculator.maxLevel(BuildingType.coralMine), 5);
     });
 
-    test('cannot upgrade when ore insufficient', () {
-      final resources = {
-        ResourceType.coral: Resource(
-          type: ResourceType.coral,
-          amount: 100,
-        ),
-        ResourceType.ore: Resource(
-          type: ResourceType.ore,
-          amount: 5,
-        ),
-      };
-      final buildings = {
-        BuildingType.headquarters:
-            Building(type: BuildingType.headquarters, level: 0),
-      };
-
-      final result = calculator.checkUpgrade(
-        type: BuildingType.headquarters,
-        currentLevel: 0,
-        resources: resources,
-        allBuildings: buildings,
-      );
-
-      expect(result.canUpgrade, isFalse);
-      expect(result.missingResources[ResourceType.ore], 15);
+    test('oreExtractor is 5', () {
+      expect(calculator.maxLevel(BuildingType.oreExtractor), 5);
     });
 
-    test('cannot upgrade at max level (isMaxLevel=true)', () {
-      final resources = {
-        ResourceType.coral: Resource(
-          type: ResourceType.coral,
-          amount: 10000,
-        ),
-        ResourceType.ore: Resource(
-          type: ResourceType.ore,
-          amount: 10000,
-        ),
-      };
-      final buildings = {
-        BuildingType.headquarters:
-            Building(type: BuildingType.headquarters, level: 10),
-      };
-
-      final result = calculator.checkUpgrade(
-        type: BuildingType.headquarters,
-        currentLevel: 10,
-        resources: resources,
-        allBuildings: buildings,
-      );
-
-      expect(result.canUpgrade, isFalse);
-      expect(result.isMaxLevel, isTrue);
-    });
-
-    test('canUpgrade false when both resources insufficient',
-        () {
-      final resources = {
-        ResourceType.coral: Resource(
-          type: ResourceType.coral,
-          amount: 0,
-        ),
-        ResourceType.ore: Resource(
-          type: ResourceType.ore,
-          amount: 0,
-        ),
-      };
-      final buildings = <BuildingType, Building>{};
-
-      final result = calculator.checkUpgrade(
-        type: BuildingType.headquarters,
-        currentLevel: 0,
-        resources: resources,
-        allBuildings: buildings,
-      );
-
-      expect(result.canUpgrade, isFalse);
-      expect(result.missingResources[ResourceType.coral], 30);
-      expect(result.missingResources[ResourceType.ore], 20);
+    test('solarPanel is 5', () {
+      expect(calculator.maxLevel(BuildingType.solarPanel), 5);
     });
   });
 }

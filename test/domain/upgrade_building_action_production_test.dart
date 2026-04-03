f2b3814e4c1b22ec6e10c582b86f4c9ff95e5e15
Antoine Morrier <antoine.morrier@gmail.com>
2026-04-03 23:37:26 +0200

    production-buildings#07 Add unit tests for UpgradeBuildingAction with production buildings

---
+++ test/domain/upgrade_building_action_production_test.dart
@@ -0,0 +1,150 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:abyss/domain/building.dart';
+import 'package:abyss/domain/building_type.dart';
+import 'package:abyss/domain/game.dart';
+import 'package:abyss/domain/player.dart';
+import 'package:abyss/domain/resource.dart';
+import 'package:abyss/domain/resource_type.dart';
+import 'package:abyss/domain/upgrade_building_action.dart';
+
+Game makeProductionGame({
+  int coral = 200,
+  int ore = 200,
+  int energy = 200,
+  int algae = 200,
+  int hqLevel = 1,
+  int algaeFarmLevel = 0,
+}) {
+  return Game(
+    player: Player(name: 'Test'),
+    resources: {
+      ResourceType.algae: Resource(
+        type: ResourceType.algae,
+        amount: algae,
+        productionPerTurn: 10,
+      ),
+      ResourceType.coral: Resource(
+        type: ResourceType.coral,
+        amount: coral,
+        productionPerTurn: 8,
+      ),
+      ResourceType.ore: Resource(
+        type: ResourceType.ore,
+        amount: ore,
+        productionPerTurn: 5,
+      ),
+      ResourceType.energy: Resource(
+        type: ResourceType.energy,
+        amount: energy,
+        productionPerTurn: 6,
+      ),
+      ResourceType.pearl: Resource(
+        type: ResourceType.pearl,
+        amount: 5,
+      ),
+    },
+    buildings: {
+      BuildingType.headquarters: Building(
+        type: BuildingType.headquarters,
+        level: hqLevel,
+      ),
+      BuildingType.algaeFarm: Building(
+        type: BuildingType.algaeFarm,
+        level: algaeFarmLevel,
+      ),
+      BuildingType.coralMine: Building(
+        type: BuildingType.coralMine,
+        level: 0,
+      ),
+      BuildingType.oreExtractor: Building(
+        type: BuildingType.oreExtractor,
+        level: 0,
+      ),
+      BuildingType.solarPanel: Building(
+        type: BuildingType.solarPanel,
+        level: 0,
+      ),
+    },
+  );
+}
+
+void main() {
+  group('production buildings', () {
+    test('validate succeeds for algaeFarm with sufficient resources and HQ',
+        () {
+      final game = makeProductionGame();
+      final action = UpgradeBuildingAction(
+        buildingType: BuildingType.algaeFarm,
+      );
+      final result = action.validate(game);
+      expect(result.isSuccess, isTrue);
+    });
+
+    test('validate fails for algaeFarm when HQ level too low', () {
+      final game = makeProductionGame(hqLevel: 0);
+      final action = UpgradeBuildingAction(
+        buildingType: BuildingType.algaeFarm,
+      );
+      final result = action.validate(game);
+      expect(result.isSuccess, isFalse);
+      expect(result.reason, isNotNull);
+    });
+
+    test('execute algaeFarm deducts coral and increments level', () {
+      final game = makeProductionGame();
+      final action = UpgradeBuildingAction(
+        buildingType: BuildingType.algaeFarm,
+      );
+      action.execute(game);
+      expect(game.resources[ResourceType.coral]!.amount, 180);
+      expect(game.buildings[BuildingType.algaeFarm]!.level, 1);
+    });
+
+    test('execute algaeFarm updates productionPerTurn', () {
+      final game = makeProductionGame();
+      final action = UpgradeBuildingAction(
+        buildingType: BuildingType.algaeFarm,
+      );
+      action.execute(game);
+      expect(
+        game.resources[ResourceType.algae]!.productionPerTurn,
+        15,
+      );
+    });
+
+    test('execute algaeFarm twice updates productionPerTurn cumulatively', () {
+      final game = makeProductionGame(hqLevel: 2);
+      final action = UpgradeBuildingAction(
+        buildingType: BuildingType.algaeFarm,
+      );
+      action.execute(game);
+      action.execute(game);
+      expect(
+        game.resources[ResourceType.algae]!.productionPerTurn,
+        20,
+      );
+    });
+
+    test('execute HQ does not change any productionPerTurn', () {
+      final game = makeProductionGame();
+      final action = UpgradeBuildingAction(
+        buildingType: BuildingType.headquarters,
+      );
+      action.execute(game);
+      expect(game.resources[ResourceType.algae]!.productionPerTurn, 10);
+      expect(game.resources[ResourceType.coral]!.productionPerTurn, 8);
+      expect(game.resources[ResourceType.ore]!.productionPerTurn, 5);
+      expect(game.resources[ResourceType.energy]!.productionPerTurn, 6);
+    });
+
+    test('validate fails for algaeFarm at max level 5', () {
+      final game = makeProductionGame(algaeFarmLevel: 5);
+      final action = UpgradeBuildingAction(
+        buildingType: BuildingType.algaeFarm,
+      );
+      final result = action.validate(game);
+      expect(result.isSuccess, isFalse);
+      expect(result.reason, isNotNull);
+    });
+  });
+}

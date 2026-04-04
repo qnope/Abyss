334c2023bfed0bb0e94dc9d42f8b7a983af740af
Antoine Morrier <antoine.morrier@gmail.com>
2026-04-04 23:24:01 +0200

    add-army#01 Create UnitType enum and UnitStats value class
    Add UnitType Hive enum (typeId 6) with 6 unit types and
    UnitStats immutable class with static forType lookup.

---
+++ lib/domain/unit_type.dart
@@ -0,0 +1,13 @@
+import 'package:hive/hive.dart';
+
+part 'unit_type.g.dart';
+
+@HiveType(typeId: 6)
+enum UnitType {
+  @HiveField(0) scout,
+  @HiveField(1) harpoonist,
+  @HiveField(2) guardian,
+  @HiveField(3) domeBreaker,
+  @HiveField(4) siphoner,
+  @HiveField(5) saboteur,
+}

08ccb874d7368c9d85d0f786870173d8f89c6e3f
Antoine Morrier <antoine.morrier@gmail.com>
2026-04-04 23:25:36 +0200

    add-army#03 Create UnitCostCalculator
    Add stateless calculator with recruitment costs, unlock tiers,
    and max recruitable count computation.

---
+++ lib/domain/unit_cost_calculator.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'resource.dart';
+import 'resource_type.dart';
+import 'unit_type.dart';
+
+class UnitCostCalculator {
+  Map<ResourceType, int> recruitmentCost(UnitType type) => switch (type) {
+    UnitType.scout => {
+      ResourceType.algae: 10,
+      ResourceType.coral: 5,
+    },
+    UnitType.harpoonist => {
+      ResourceType.algae: 15,
+      ResourceType.coral: 10,
+      ResourceType.ore: 5,
+    },
+    UnitType.guardian => {
+      ResourceType.coral: 20,
+      ResourceType.ore: 15,
+    },
+    UnitType.domeBreaker => {
+      ResourceType.ore: 25,
+      ResourceType.energy: 15,
+    },
+    UnitType.siphoner => {
+      ResourceType.algae: 20,
+      ResourceType.energy: 10,
+      ResourceType.pearl: 2,
+    },
+    UnitType.saboteur => {
+      ResourceType.coral: 15,
+      ResourceType.energy: 20,
+      ResourceType.pearl: 3,
+    },
+  };
+
+  int unlockLevel(UnitType type) => switch (type) {
+    UnitType.scout || UnitType.harpoonist => 1,
+    UnitType.guardian || UnitType.domeBreaker => 3,
+    UnitType.siphoner || UnitType.saboteur => 5,
+  };
+
+  bool isUnlocked(UnitType type, int barracksLevel) =>
+      barracksLevel >= unlockLevel(type);
+
+  int maxRecruitableCount(
+    UnitType type,
+    int barracksLevel,
+    Map<ResourceType, Resource> resources,
+  ) {
+    final costs = recruitmentCost(type);
+    var minAffordable = barracksLevel * 100;
+
+    for (final entry in costs.entries) {
+      final available = resources[entry.key]?.amount ?? 0;
+      final affordable = available ~/ entry.value;
+      minAffordable = min(minAffordable, affordable);
+    }
+
+    return max(0, minAffordable);
+  }
+}

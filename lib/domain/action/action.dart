2d4bbc97566f37170d60f3f4711a591b5f3d5cb4
Antoine Morrier <antoine.morrier@gmail.com>
2026-04-08 12:16:23 +0200

    game-player-decoupling#06 Update Action contract to receive Player
    Action.validate and Action.execute now take a Player alongside Game.
    ActionExecutor forwards the player. Concrete actions will be migrated
    in tasks 07-11.

--- lib/domain/action/action.dart
+++ lib/domain/action/action.dart
@@ -1,10 +1,11 @@
 import 'action_result.dart';
 import 'action_type.dart';
 import '../game/game.dart';
+import '../game/player.dart';
 
 abstract class Action {
   ActionType get type;
   String get description;
-  ActionResult validate(Game game);
-  ActionResult execute(Game game);
+  ActionResult validate(Game game, Player player);
+  ActionResult execute(Game game, Player player);
 }
--- lib/domain/action/action_executor.dart
+++ lib/domain/action/action_executor.dart
@@ -1,11 +1,12 @@
 import 'action.dart';
 import 'action_result.dart';
 import '../game/game.dart';
+import '../game/player.dart';
 
 class ActionExecutor {
-  ActionResult execute(Action action, Game game) {
-    final validation = action.validate(game);
+  ActionResult execute(Action action, Game game, Player player) {
+    final validation = action.validate(game, player);
     if (!validation.isSuccess) return validation;
-    return action.execute(game);
+    return action.execute(game, player);
   }
 }

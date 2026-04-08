4671acdc6c277f3ee935badccd567e49f2222a94
Antoine Morrier <antoine.morrier@gmail.com>
2026-04-08 12:14:55 +0200

    game-player-decoupling#02 Extend Player model with per-player state
    Add id, baseX/Y, resources, buildings, techBranches, units,
    recruitedUnitTypes, pendingExplorations, revealedCells fields to Player.
    Add Player.withBase named constructor that computes initial reveal area.
    Move default state factories into player_defaults.dart to keep
    player.dart under 150 lines.

---
+++ lib/domain/game/player_defaults.dart
@@ -0,0 +1,68 @@
+import '../building/building.dart';
+import '../building/building_type.dart';
+import '../resource/resource.dart';
+import '../resource/resource_type.dart';
+import '../tech/tech_branch.dart';
+import '../tech/tech_branch_state.dart';
+import '../unit/unit.dart';
+import '../unit/unit_type.dart';
+
+class PlayerDefaults {
+  const PlayerDefaults._();
+
+  static Map<ResourceType, Resource> resources() {
+    return {
+      ResourceType.algae: Resource(
+        type: ResourceType.algae,
+        amount: 100,
+        maxStorage: 5000,
+      ),
+      ResourceType.coral: Resource(
+        type: ResourceType.coral,
+        amount: 80,
+        maxStorage: 5000,
+      ),
+      ResourceType.ore: Resource(
+        type: ResourceType.ore,
+        amount: 50,
+        maxStorage: 5000,
+      ),
+      ResourceType.energy: Resource(
+        type: ResourceType.energy,
+        amount: 60,
+        maxStorage: 1000,
+      ),
+      ResourceType.pearl: Resource(
+        type: ResourceType.pearl,
+        amount: 5,
+        maxStorage: 100,
+      ),
+    };
+  }
+
+  static Map<TechBranch, TechBranchState> techBranches() {
+    return {
+      TechBranch.military: TechBranchState(branch: TechBranch.military),
+      TechBranch.resources: TechBranchState(branch: TechBranch.resources),
+      TechBranch.explorer: TechBranchState(branch: TechBranch.explorer),
+    };
+  }
+
+  static Map<BuildingType, Building> buildings() {
+    return {
+      BuildingType.headquarters: Building(type: BuildingType.headquarters, level: 0),
+      BuildingType.algaeFarm: Building(type: BuildingType.algaeFarm, level: 0),
+      BuildingType.coralMine: Building(type: BuildingType.coralMine, level: 0),
+      BuildingType.oreExtractor: Building(type: BuildingType.oreExtractor, level: 0),
+      BuildingType.solarPanel: Building(type: BuildingType.solarPanel, level: 0),
+      BuildingType.laboratory: Building(type: BuildingType.laboratory, level: 0),
+      BuildingType.barracks: Building(type: BuildingType.barracks, level: 0),
+    };
+  }
+
+  static Map<UnitType, Unit> units() {
+    return {
+      for (final type in UnitType.values) type: Unit(type: type),
+    };
+  }
+}

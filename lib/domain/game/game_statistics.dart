8cc47f98a693acb0ae0cf7b01b5da1fde639257a
Antoine Morrier <antoine.morrier@gmail.com>
2026-04-12 18:23:30 +0200

    end-game#13 Create GameStatisticsCalculator

---
+++ lib/domain/game/game_statistics.dart
@@ -0,0 +1,13 @@
+class GameStatistics {
+  final int turnsPlayed;
+  final int monstersDefeated;
+  final int basesCaptured;
+  final int totalResourcesCollected;
+
+  const GameStatistics({
+    required this.turnsPlayed,
+    required this.monstersDefeated,
+    required this.basesCaptured,
+    required this.totalResourcesCollected,
+  });
+}
